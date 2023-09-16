a7d454d8d6221011422a2a6abd9202287ff66967
chenxi sun <130527404+SunChenxi-Glc@users.noreply.github.com>
2023-09-17 03:07:54 +0800

    SCX Update cachematrix.R
    WEEK3 QUIZ

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,26 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+         cache <- NULL
+  
+  setMatrix <- function(matrix) {
+    x <<- matrix
+    cache <<- NULL
+  }
+  
+  getMatrix <- function() {
+    x
+  }
+  
+  setCache <- function(inverse) {
+    cache <<- inverse
+  }
+
+  getCache <- function() {
+    cache
+  }
+
+  list(setMatrix = setMatrix, getMatrix = getMatrix, setCache = setCache, getCache = getCache)
 
 }
 
@@ -12,4 +32,17 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        cache <- x$getCache()
+  
+  if (!is.null(cache)) {
+    message("Getting cached data")
+    return(cache)
+  }
+
+  data <- x$getMatrix()
+  inverse <- solve(data, ...)
+  
+  x$setCache(inverse)
+  
+  inverse
 }
